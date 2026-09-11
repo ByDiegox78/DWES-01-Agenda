60280e6fd722e6c6708b50e3b0022c597ae709ae
ByDiegox78 <oms04081974@gmail.com>
2026-09-11 16:39:13 +0200

    Añadido modelo

---
+++ Agenda/Agenda/Models/Contacto.cs
@@ -0,0 +1,12 @@
+﻿namespace Agenda.Models;
+
+public record Contacto() {
+    public int Id { get; set; }
+    public string Nombre { get; init; }
+    public string Alias { get; init; }
+    public int Telefono { get; init; }
+    public string Email { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; init; }
+    public bool IsDeleted { get; init; }
+}
