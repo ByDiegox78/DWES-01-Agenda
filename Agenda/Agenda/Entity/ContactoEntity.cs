b7383672009093eccf4cb863ed4e73666b5e66dc
ByDiegox78 <oms04081974@gmail.com>
2026-09-12 00:40:31 +0200

    Terminado Entity

---
+++ Agenda/Agenda/Entity/ContactoEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Agenda.Entity;
+
+[Table("Contacto")]
+
+public class ContactoEntity {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string Nombre { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string Alias { get; set; }
+    [Required]
+    public int Telefono { get; set; }
+    [Required]  
+    [MaxLength(100)]
+    public string Email { get; set; }
+}
